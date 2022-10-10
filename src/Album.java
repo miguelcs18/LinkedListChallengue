dcb424e1ff17f403913342bb9837055ec998d169
miguelcs18 <miguelcs18@gmail.com>
2022-10-10 09:42:06 -0500

    Add Album class constructor

--- src/Album.java
+++ src/Album.java
@@ -4,4 +4,13 @@ import java.util.LinkedList;
 public class Album {
     private LinkedList<Song> songsList;
     private ArrayList<Album> albumList;
+
+    public Album(LinkedList<Song> songsList, ArrayList<Album> albumList, String albumName, String songName) {
+        this.songsList = new LinkedList<>();
+        this.albumList = new ArrayList<>();
+    }
+
+
+
+
 }
