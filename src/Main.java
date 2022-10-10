3c8d1a8f8b85405949ab146165f7c44ac27770ca
miguelcs18 <miguelcs18@gmail.com>
2022-10-10 10:55:29 -0500

    Add test data in main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Album  albumTest = new Album();
+        albumTest.addAlbum("Van Haleen");
+        albumTest.addAlbum("Black Sabbath");
     }
 }
